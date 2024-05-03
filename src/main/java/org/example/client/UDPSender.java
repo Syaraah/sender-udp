2c65f21cc47c870167a2c0e40320d0dbf1ec76a4
Syaraah <163830582+Syaraah@users.noreply.github.com>
2024-05-03 10:20:22 +0700

    Update UDPSender.java

--- src/main/java/org/example/client/UDPSender.java
+++ src/main/java/org/example/client/UDPSender.java
@@ -9,14 +9,19 @@ public class UDPSender {
     public void sendMessage() throws IOException {
         DatagramSocket socket = new DatagramSocket();
 
-        String message = "Hello, Server!";
-        byte[] buffer = message.getBytes();
+        //Joystick
+        String joystickMessage = "111010000000100000000000111100111001111100011000000000011110010101110000000000000000000000000000";
+        byte[] joystickBuffer = joystickMessage.getBytes();
+        InetAddress joystickAddress = InetAddress.getByName("192.168.225.133");
+        DatagramPacket joystickPacket = new DatagramPacket(joystickBuffer, joystickBuffer.length, joystickAddress, 9876);
+        socket.send(joystickPacket);
 
-        InetAddress address = InetAddress.getByName( "192.168.25.133");
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 12345);
-
-        socket.send(packet);
-        socket.receive(packet);
+        //SystemTarget
+        String systemTargetMessage = "1111110000001000000000000010110000110011110000000010000010000000110101111111010111110111000001111010101001001110010111001101001110011111110110000010111100100010001110010101010000110101001101010011010111001010";
+        byte[] systemTargetBuffer = systemTargetMessage.getBytes();
+        InetAddress systemTargetAddress = InetAddress.getByName( "192.168.225.133");
+        DatagramPacket systemTargetPacket = new DatagramPacket(systemTargetBuffer, systemTargetBuffer.length, systemTargetAddress, 9876);
+        socket.send(systemTargetPacket);
 
         System.out.println("Received: " + new String(packet.getData()));
         socket.close();
